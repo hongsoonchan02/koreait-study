7e3a76bdafbab4a73b14ec7d8f954ad4060835d1
hongsoonchan02 <asm640789@gmail.com>
2026-02-24 18:19:40 +0900

    26-02-24

---
+++ 4.Spring/restStudy/src/main/java/kr/co/restStudy/board/dto/ResBoardDTO.java
@@ -0,0 +1,28 @@
+package kr.co.restStudy.board.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ResBoardDTO {
+	private Long id;
+	private String title;
+	private String content;
+	private String writerName;
+	private String category;
+	private LocalDateTime createdAt;
+	private int viewCount;
+	private List<ResBoardFileDTO> files;
+
+}
